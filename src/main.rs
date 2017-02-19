36f7de969de4dffbc2a46fd85e256b377d02ec25
James Forcier <csssuf@csssuf.net>
2017-02-19 17:45:47 -0500

    Add initial /announce implementation
    Currently tracks what torrents have been announced, but doesn't track or send
    peer lists.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,24 @@ extern crate iron;
 extern crate rustc_serialize;
 extern crate bencode;
 extern crate params;
+extern crate persistent;
 
+use std::collections::HashMap;
+use std::ops::Deref;
 use iron::prelude::*;
+use iron::typemap::Key;
 use iron::status;
-use router::Router;
-use bencode::encode;
+use persistent::State;
 
 mod proto;
+mod tracker;
+
+#[derive(Clone, Copy)]
+pub struct TorrentList;
+
+impl Key for TorrentList {
+    type Value = HashMap<String, tracker::Torrent>;
+}
 
 fn main() {
     let router = router!{
@@ -17,7 +28,10 @@ fn main() {
         announce: get "/announce" => announce_handler
     };
 
-    Iron::new(router).http("localhost:3000").unwrap();
+    let mut chain = Chain::new(router);
+    chain.link(State::<TorrentList>::both(HashMap::new()));
+
+    Iron::new(chain).http("0.0.0.0:3000").unwrap();
 }
 
 fn root_handler(r: &mut Request) -> IronResult<Response> {
@@ -27,7 +41,59 @@ fn root_handler(r: &mut Request) -> IronResult<Response> {
 
 fn announce_handler(r: &mut Request) -> IronResult<Response> {
     use params::{Params, Value};
-    let params = r.get_ref::<Params>().unwrap();
+
+    let mut result: Vec<u8> = vec![];
+    
+    let params = r.get::<Params>().unwrap();
+    let mutex = r.get_mut::<State<TorrentList>>().unwrap();
     println!("{:?}", params);
-    Ok(Response::with((status::Ok, proto::generate_failure("Unimplemented"))))
+
+    match params.find(&["info_hash"]) {
+        Some(&Value::String(ref info_hash)) => {
+            let mut done = false;
+            {
+                let _list_ro = mutex.read().unwrap();
+                let list_ro = _list_ro.deref();
+
+                match list_ro.get(info_hash) {
+                    Some(torrent) => {
+                        println!("Torrent found: {:?}", torrent);
+                        result = proto::generate_failure("It worked but peer lists are still unimplemented");
+                        done = true;
+                    },
+                    None => {
+                        println!("Torrent not found, adding.");
+                    }
+                }
+            }
+            if !done {
+                let mut new_torrent = tracker::Torrent::new(info_hash.clone());
+                
+                let new_peer = tracker::RTPeer::new(
+                    params.find(&["peer_id"]),
+                    params.find(&["port"]),
+                    params.find(&["uploaded"]),
+                    params.find(&["downloaded"]),
+                    params.find(&["left"]),
+                    Some("".to_string()),
+                    params.find(&["key"])
+                );
+
+                match new_peer {
+                    Ok(val) => {
+                        new_torrent.peers.push(val);
+                        let mut list_rw = mutex.write().unwrap();
+                        list_rw.insert(info_hash.clone(), new_torrent);
+                        result = proto::generate_failure("It probably actually worked");
+                    },
+                    _ => { result = proto::generate_failure("Error generating peer"); }
+                }
+            }
+        },
+        _ => {
+            result = proto::generate_failure("No info_hash specified");
+        }
+    }
+
+    Ok(Response::with((status::Ok, result)))
 }
---
+++ src/tracker.rs
@@ -0,0 +1,104 @@
+use params;
+use params::{Value, FromValue};
+
+#[derive(Debug)]
+pub struct Torrent {
+    pub info_hash: String,
+    pub peers: Vec<RTPeer>
+}
+
+impl Torrent {
+    pub fn new(info_hash: String) -> Torrent {
+        Torrent {
+            info_hash: info_hash,
+            peers: Vec::new()
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct RTPeer {
+    peer_id: String,
+    port: u16,
+    uploaded: u32,
+    downloaded: u32,
+    left: u32,
+    ip: String,
+    key: String
+}
+
+impl RTPeer {
+    pub fn new(peer_id: Option<&params::Value>,
+           port: Option<&params::Value>,
+           uploaded: Option<&params::Value>,
+           downloaded: Option<&params::Value>,
+           left: Option<&params::Value>,
+           ip: Option<String>,
+           key: Option<&params::Value>) -> Result<RTPeer, &'static str> {
+        let _peer_id: String;
+        let _port: u16;
+        let _uploaded: u32;
+        let _downloaded: u32;
+        let _left: u32;
+        let _ip: String;
+        let _key: String;
+
+        match peer_id {
+            Some(&Value::String(ref found)) => { _peer_id = found.clone(); },
+            _ => { return Err("No peer_id provided"); }
+        }
+        match port {
+            Some(found) => { 
+                match u16::from_value(found) {
+                    Some(converted) => { _port = converted; },
+                    _ => { return Err("No port provided"); }
+                }
+            },
+            _ => { return Err("No port provided"); }
+        }
+        match uploaded {
+            Some(found) => { 
+                match u32::from_value(found) {
+                    Some(converted) => { _uploaded = converted; },
+                    _ => { _uploaded = 0; }
+                }
+            },
+            _ => { _uploaded = 0; }
+        }
+        match downloaded {
+            Some(found) => {
+                match u32::from_value(found) {
+                    Some(converted) => { _downloaded = converted; },
+                    _ => { _downloaded = 0; }
+                }
+            },
+            _ => { _downloaded = 0; }
+        }
+        match left {
+            Some(found) => {
+                match u32::from_value(found) {
+                    Some(converted) => { _left = converted; },
+                    _ => { return Err("Invalid value for 'left'"); }
+                }
+            },
+            _ => { return Err("No left provided"); }
+        }
+        match ip {
+            Some(found) => { _ip = found; },
+            _ => { return Err("No ip provided"); }
+        }
+        match key {
+            Some(&Value::String(ref found)) => { _key = found.clone(); }
+            _ => { _key = "".to_string(); }
+        }
+        Ok(RTPeer {
+            peer_id: _peer_id,
+            port: _port,
+            uploaded: _uploaded,
+            downloaded: _downloaded,
+            left: _left,
+            ip: _ip,
+            key: _key
+        })
+    }
+}
