a1a436ddd372234e6d9858d62ffa34f44cec2295
James Forcier <csssuf@csssuf.net>
2017-02-19 21:52:39 -0500

    Add non-first peers to peer list
    The peer list still accumulates duplicate peers as the peers' statuses change.

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@ fn announce_handler(r: &mut Request) -> IronResult<Response> {
 
     match params.find(&["info_hash"]) {
         Some(&Value::String(ref info_hash)) => {
-            let mut done = false;
+            let mut found = false;
             {
                 let _list_ro = mutex.read().unwrap();
                 let list_ro = _list_ro.deref();
@@ -61,49 +61,70 @@ fn announce_handler(r: &mut Request) -> IronResult<Response> {
                 match list_ro.get(info_hash) {
                     Some(torrent) => {
                         println!("Torrent found: {:?}", torrent);
-                        // Calculate complete and incomplete
-                        let _peer_copy = torrent.peers.to_vec();
-                        let complete = _peer_copy.into_iter().filter(|v| v.left == 0).collect::<Vec<_>>().len();
-                        let incomplete = torrent.peers.len() - complete;
-
-                        // build result struct
-                        let result_struct = proto::RTResponse {
-                            interval: 3600,
-                            tracker_id: "something".to_string(),
-                            complete: complete as u32,
-                            incomplete: incomplete as u32,
-                            peers: torrent.peers.to_vec().into_iter().map(|v| proto::Peer { peer_id: v.peer_id, ip: v.ip, port: v.port }).collect::<Vec<_>>()
-                        };
-                        result = encode(result_struct).unwrap();
-                        done = true;
+                        found = true;
                     },
                     None => {
                         println!("Torrent not found, adding.");
                     }
                 }
             }
-            if !done {
-                let mut new_torrent = tracker::Torrent::new(info_hash.clone());
-                
-                let new_peer = tracker::RTPeer::new(
-                    params.find(&["peer_id"]),
-                    params.find(&["port"]),
-                    params.find(&["uploaded"]),
-                    params.find(&["downloaded"]),
-                    params.find(&["left"]),
-                    Some(util::sock2str(&remote_addr)),
-                    params.find(&["key"])
-                );
 
-                match new_peer {
-                    Ok(val) => {
-                        new_torrent.peers.push(val);
-                        let mut list_rw = mutex.write().unwrap();
+            let new_peer = tracker::RTPeer::new(
+                params.find(&["peer_id"]),
+                params.find(&["port"]),
+                params.find(&["uploaded"]),
+                params.find(&["downloaded"]),
+                params.find(&["left"]),
+                Some(util::sock2str(&remote_addr)),
+                params.find(&["key"])
+            );
+
+            match new_peer {
+                Ok(val) => {
+                    let mut list_rw = mutex.write().unwrap();
+
+                    if !found {
+                        let mut new_torrent = tracker::Torrent::new(info_hash.clone());
+                        new_torrent.peers.push(val.clone());
                         list_rw.insert(info_hash.clone(), new_torrent);
-                        result = proto::generate_failure("It probably actually worked");
-                    },
-                    _ => { result = proto::generate_failure("Error generating peer"); }
-                }
+                    }
+
+                    match list_rw.get_mut(info_hash) {
+                        Some(torrent) => {
+                            if found {
+                                torrent.peers.push(val.clone());
+                            }
+
+                            let complete = torrent.peers.to_vec()
+                                                        .into_iter()
+                                                        .filter(|v| v.left == 0)
+                                                        .collect::<Vec<_>>()
+                                                        .len();
+                            let incomplete = torrent.peers.len() - complete;
+
+                            let result_struct = proto::RTResponse {
+                                interval: 1800,
+                                tracker_id: "something".to_string(),
+                                complete: complete as u32,
+                                incomplete: incomplete as u32,
+                                peers: torrent.peers.to_vec()
+                                                    .into_iter()
+                                                    .map(|v| proto::Peer {
+                                                        peer_id: v.peer_id,
+                                                        ip: v.ip,
+                                                        port: v.port
+                                                    })
+                                                    .collect::<Vec<_>>()
+                            };
+                            result = encode(result_struct).unwrap();
+                        },
+                        None => {
+                            result = proto::generate_failure("Couldn't get torrent info - something went horribly wrong.");
+                        }
+                    }
+
+                },
+                _ => { result = proto::generate_failure("Error generating peer"); }
             }
         },
         _ => {
